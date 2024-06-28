3820d156ec485f861e9aa18527fc268cfcf0af18
IgorShevtshenko <igorse2001@gmail.com>
2024-06-28 19:41:04 +0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "git@github.com:IgorShevtshenko/Utils.git",
-            .upToNextMajor(from: .init(1, 1, 0))
+            .upToNextMajor(from: .init(1, 1, 1))
         ),
     ],
     targets: [
